b8dd2efd92d2045f76499c91f9bbf939657837e0
Ubuntu <ubuntu@ip-172-31-31-243.us-west-2.compute.internal>
2018-02-05 10:53:20 +0000

    Twitter and Twillio working, DB not

--- APIRequests.php
+++ APIRequests.php
@@ -1,42 +1,42 @@
 <?php
 $isTweet=$_GET['tweet'];
 $isTwillio=$_GET['twillio'];
-
+// echo "HELLO";
 use Twilio\Rest\Client;
 if ($isTweet) {
+// echo "E?";
     $client = new http\Client;
     $request = new http\Client\Request;
-
+// echo "EM?";
     $request->setRequestUrl('https://api.twitter.com/1.1/statuses/user_timeline.json');
     $request->setRequestMethod('GET');
     $request->setQuery(new http\QueryString(array(
         'count' => '1',
         'screen_name' => 'Dziastis'
     )));
-
+// echo "here1";
     $request->setHeaders(array(
-  'authorization' => 'OAuth oauth_consumer_key="660PKn9qD6wnTyARbolTUKMEJ", oauth_nonce="e8TStetU54nzI4d3THlCXq60hjSJpVEI", oauth_signature="UNleuCMT6k9Yy6DdmAasdntSyyU%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1517580449", oauth_token="4527318323-QgDLuD3iPMTlvYTTYSrroMigkmwH97IcXcYidvx", oauth_version="1.0"'
+  'authorization' => 'OAuth oauth_consumer_key="660PKn9qD6wnTyARbolTUKMEJ", oauth_nonce="E2ukO9PaV4VosjP7w78bCK8IkZje2yDK", oauth_signature="sO%2FrnnunxqnGO049kU7kf6%2B72Zs%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1517685861", oauth_token="4527318323-QgDLuD3iPMTlvYTTYSrroMigkmwH97IcXcYidvx", oauth_version="1.0"'
 ));
-
-
+// echo "here2";
     $client->setCookies(array(
         'guest_id' => 'v1%3A151622209740528028',
         'lang' => 'en',
         'personalization_id' => '"v1_C135iIQD0UsAYB+o4gxOaw=="'
     ));
-
+// echo "here3";
     $client->enqueue($request)->send();
     $response = $client->getResponse();
-//echo "1";
+// echo "1";
     $json = $response->getBody();
-//echo $json;
+// echo $json;
     $tweets = json_decode($json, true);
 
     echo $tweets[0]['text'];
 }elseif ($isTwillio){
     // Required if your environment does not handle autoloading
     require __DIR__ . '/vendor/autoload.php';
-
+// echo "Macna gyvate";
     // Use the REST API Client to make requests to the Twilio REST API
 
 
@@ -44,7 +44,7 @@ if ($isTweet) {
     $sid = 'AC13e4b5e82ecd12c5c1a60a523c0843dd';
     $token = '93d910e4ff352ca25a142565e90d2a63';
     $client = new Client($sid, $token);
-
+print_r($client->messages);
     // Use the client to do fun stuff like send text messages!
     $client->messages->create(
     // the number you'd like to send the message to
@@ -56,6 +56,7 @@ if ($isTweet) {
             'body' => 'Hello from NodeMCU!'
         )
     );
+// echo $client;
     echo "SMS sent";
 
 }
